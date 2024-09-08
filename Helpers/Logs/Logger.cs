caa8ad84677ef556dba87883038754213d4242b7
DanielDobosh <danieldobosh362@gmail.com>
2024-09-08 21:05:56 +0300

    Fix issues with logger

--- Helpers/Logs/Logger.cs
+++ Helpers/Logs/Logger.cs
@@ -12,7 +12,7 @@ namespace testProd
             DateTime dateTime = DateTime.Now;
             string datalog = dateTime.ToString("yyyy-MM-dd HH:mm:ss");
 
-            _logger.LogInformation("{Timestamp}: " + message, datalog, args);
+            _logger.LogInformation("{Timestamp}: " + message + datalog + args);
         }
 
         public void LogWarning(string message, params object?[] args)
@@ -20,7 +20,7 @@ namespace testProd
             DateTime dateTime = DateTime.Now;
             string datalog = dateTime.ToString("yyyy-MM-dd HH:mm:ss");
 
-            _logger.LogWarning("{Timestamp}: " + message, datalog, args);
+            _logger.LogWarning("{Timestamp}: " + message + datalog + args);
         }
 
     }
--- obj/Debug/net8.0/TestAssignment.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestAssignment.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestAssignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7e69d43fd53f896f8153f63396609a1f1451392")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb1ed869498fffaa82b4e343a867abe63712d271")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestAssignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestAssignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
